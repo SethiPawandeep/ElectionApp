9da2c0f94faa9005c6e138eb87382fba6f1fa16c
pawandeep2007 <pawandeep2007@gmail.com>
2016-02-26 20:06:50 +0530

    vote page

--- Election-UI/www/js/views/vote.js
+++ Election-UI/www/js/views/vote.js
@@ -1,12 +1,14 @@
 define(['marionette', 'tpl!templates/vote'], function (Marionette, tplVote) {
     var voteListChild: Marionette.ItemView.extend({
         template: tplVote,
+        templateHelpers: function({
+            candidateListSize: this.options.candidateList.length,
+            candidateList: this.option.candidateList
+            
+            return {candidateListSize}, {candidateList};
+        });
         className: 'row-well'
     });
 
-    var voteList: Marionette.CollectionView.extend({
-        childView: voteListChild
-    });
-
-    return voteList;
+    return voteListChild;
 });
